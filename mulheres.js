9a3d0323d09223cb17e1baa221b584271d5d2a22
Laissa Cunha <laissa.cunha@ed3.com.br>
2023-12-10 10:33:28 -0300

    feat: adiciona comentários ao código.

--- mulheres.js
+++ mulheres.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express'); // Inicia o Express
+const router = express.Router(); // Configura a primeira parte da rota
 
-const app = express();
-const porta = 3333;
+const app = express(); // Inicia o App
+const porta = 3333; // Cria a porta
 
+// Cria lista inicial de mulheres - estática
 const mulheres = [
   {
     nome: 'Simara Conceição',
@@ -22,14 +23,15 @@ const mulheres = [
 
   }
 ]
-
-console.log( typeof mulheres)
+// Função relacionada ao método get
 function mostraMulheres(request, response) {
   response.json(mulheres)
 }
 
+// Configura rota 'get'
 app.use(router.get('/mulheres', mostraMulheres));
 
+//  Servidor ouvindo a porta - função anônima
 app.listen(porta, () => {
   console.log(`Servidor criado e rodando na porta: ${porta}.`);
 })
