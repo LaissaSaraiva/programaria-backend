a09a6fa94e15f0e6abc38572c45fccf6e0742f35
Laissa Cunha <laissa.cunha@ed3.com.br>
2024-01-16 10:57:28 -0300

    refactor: adiciona Model em servidos e refatora função mostraMulheres

--- mulheres.js
+++ mulheres.js
@@ -5,37 +5,20 @@ const { v4: uuidv4 } = require('uuid'); // Inicia a Uuid
 const conectaBancoDeDados = require('./banco-de-dados') //Conecta o servidor ao arquivo banco-de-dados.js
 conectaBancoDeDados(); //Chama função.
 
+const Mulher = require('./mulherModel') // Chama o Model Mulher - regras da criação do Objeto Mulher.
 const app = express(); // Inicia o App
 app.use(express.json()); // Informa que a partir de agora os dados serão enviados em formato json
 const porta = 3333; // Cria a porta
-
-// Cria lista inicial de mulheres - estática
-const mulheres = [
-  {
-    id: '1',
-    nome: 'Simara Conceição',
-    imagem: 'https://github.com/simaraconceicao.png',
-    minibio: 'Desenvolvedora e instrutora'
-  },
-  {
-    id: '2',
-    nome: 'Iana Chan',
-    imagem: 'https://github.com/simaraconceicao.png',
-    minibio: 'Fundadora e CEO da Programaria'
-  },
-  {
-    id: '3',
-    nome: 'Nat F Dev',
-    imagem: 'https://github.com/simaraconceicao.png',
-    minibio: 'Artista de CSS'
-
-  }
-]
 //  OBS: Toda função atrelada a rota receber por parametros (request, response)
 
 // GET - Função relacionada ao método get
-function mostraMulheres(request, response) {
-  response.json(mulheres)
+async function mostraMulheres(request, response) {
+  try {
+    const mulheresVindasDoBancoDeDados = await Mulher.find() // Quando a conexão acontecer eu quero buscar do banco de dados a lista de divas.
+    response.json(mulheresVindasDoBancoDeDados)
+  } catch(erro) {
+    console.log(erro)    
+  }
 }
 
 // POST
